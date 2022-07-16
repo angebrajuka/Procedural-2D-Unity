925397ac261b2a563391978a7b038d67d1a8b750
angelobrajuka <brajuka.angelo@gmail.com>
2022-07-15 21:42:51 -0400

    stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,9 @@ public class PlayerMovement : MonoBehaviour {
     private Collider2D c2d;
     private float halfColliderHeight;
 
+    private int inX, inY;
+    private bool sneak;
+
     public int X { get { return (int)Mathf.Floor(rb.position.x + c2d.offset.x); } }
     public int Y { get { return (int)Mathf.Floor(rb.position.y + c2d.offset.y - halfColliderHeight); } }
     public bool InWater { get { return worldLoading.IsWater(X, Y); } }
@@ -32,15 +35,18 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Update() {
-        rb.velocity = new Vector2(
-            (PlayerInput.GetKey("move east" ) ? 1 : 0) + (PlayerInput.GetKey("move west" ) ? -1 : 0),
-            (PlayerInput.GetKey("move north") ? 1 : 0) + (PlayerInput.GetKey("move south") ? -1 : 0)
-        ).normalized * (InWater ? waterSpeed : (PlayerInput.GetKey("sneak") ? walkSpeed : runSpeed));
+        inX = (PlayerInput.GetKey("move east" ) ? 1 : 0) + (PlayerInput.GetKey("move west" ) ? -1 : 0);
+        inY = (PlayerInput.GetKey("move north") ? 1 : 0) + (PlayerInput.GetKey("move south") ? -1 : 0);
+        sneak = (PlayerInput.GetKey("sneak"));
 
         pAnimator.UpdateMovement(
             (rb.velocity.x == 0) ? (Input.mousePosition.x > (Screen.width / 2) ? false : true) : (rb.velocity.x > 0 ? false : true),
             Moving,
             rb.velocity.magnitude
         );
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(inX, inY).normalized * (InWater ? waterSpeed : sneak ? walkSpeed : runSpeed);
     }
 }
